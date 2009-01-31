191043f3fb60f729785adc76569c9a23dbebc86c
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2009-01-31 20:33:50 -0200

    Fixed name of attributes persisted in contracts

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -32,10 +32,10 @@ class ContractsController < ApplicationController
     debugger
     @contract_fields = ""
     params[:contract].each do |field|
-      @contract_fields << field[0] + ": " + field[1] + ", "
+      @contract_fields << "{" + field[0] + ": " + field[1] + "}, "
     end
     
-    params[:contract] = {:contract_category_id => params[:contract_category_id], :contract_type_id => '1', :contract => @contract_fields}
+    params[:contract] = {:contract_category_id => params[:contract_category].to_a[0][1], :contract_type_id => params[:contract_type_hidden], :contract => @contract_fields}
     @contract = Contract.new(params[:contract])
 
     respond_to do |format|
